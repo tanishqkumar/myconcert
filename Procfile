ead02ab3ac82e8b7a6cf955653a0551deb210186
tanishqkumar <thetanishq@gmail.com>
2020-05-05 12:54:05 +0400

    deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn myconcert.wsgi --log-file -
--- myconcert/settings.py
+++ myconcert/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'l-0e%82(q35dk+uqk22=!jyedg*365-3=qb4cguh0u2(#e=5t$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['myconcert.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
